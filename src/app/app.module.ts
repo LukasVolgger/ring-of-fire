5a0ca77d30a48b45a0e0ee391f06d604173d646b
Lukas Volgger <lukas.volgger@live.at>
2022-10-20 13:33:40 +0200

    install angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import { MatInputModule } from '@angular/material/input';
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
 import { MatCardModule } from '@angular/material/card';
 import { GameInfoComponent } from './game-info/game-info.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -34,7 +39,11 @@ import { GameInfoComponent } from './game-info/game-info.component';
     MatIconModule,
     MatDialogModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-cfd93',
+    appId: '1:526037969321:web:5d8b2fe8c4ccdef691bcb8',
+    storageBucket: 'ring-of-fire-cfd93.appspot.com',
+    apiKey: 'AIzaSyAkeH67Yn9egVLumK1O7H6NgiGKRyWu1UM',
+    authDomain: 'ring-of-fire-cfd93.firebaseapp.com',
+    messagingSenderId: '526037969321',
+  },
   production: true
 };
