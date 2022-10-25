c00855b666fa8b5f07996fb4a570d843be59ceb6
Lukas Volgger <lukas.volgger@live.at>
2022-10-25 14:05:06 +0200

    development paused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ring-of-fire';
 }
+
+// ########################################################################################### DEVELOPMENT PAUSED
+// Paused on 25.10.2022
+// FIXME Avoid drawing cards before a player name was added
+// TODO Limit players to e.g. 4
+// TODO Add dialog to edit/delete player! Edit: title, img | delete: player
+// TODO Implement game over function and screen when drawing last card from stack
+// TODO Implement game settings screen => Change bg (SVG's already exist), change lang....
+// TODO Implement 'Share Game'-Button to share the gameID via email and play with others
+// TODO Responsive design
