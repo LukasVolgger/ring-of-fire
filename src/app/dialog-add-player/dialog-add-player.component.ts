837088cafc623664f1edfd204207e7dcecc31aba
Lukas Volgger <lukas.volgger@live.at>
2022-11-07 10:04:20 +0100

    remove unneccessary function

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-player',
@@ -12,13 +12,6 @@ export class DialogAddPlayerComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { }
 
   ngOnInit(): void {
-  }
-
-  /**
-   * Close dialog
-   */
-  onNoClick() {
-    this.dialogRef.close();
   }
 
 }
